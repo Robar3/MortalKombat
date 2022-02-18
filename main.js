d260c9621d0e14156b5a71a167939d1bdf9d5bf1
Иван <terehov94@mail.ru>
2022-02-18 03:10:36 +0300

    add 5 homework

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const $arena = document.querySelector('.arenas');
 function attack(){
     console.log(this.name +' Fight...');
 }
-function getRandom(){
-    return Math.ceil(Math.random()*20);
+function getRandom(random){
+    return Math.ceil(Math.random()*random);
 }
-const $randomButton= document.querySelector('.button');
+// const $randomButton= document.querySelector('.button');
+const  $formFight = document.querySelector('.control');
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+const ATTACK = ['head', 'body', 'foot'];
 const player1 ={
     player: 1,
     name: 'Sonya',
@@ -13,9 +20,9 @@ const player1 ={
     img:'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon:['axe','knife'],
     attack:attack,
-    changeHP:changeHP,
-    elHP:elHP,
-    renderHP:renderHP
+    changeHP,
+    elHP,
+    renderHP
 };
 player1.attack();
 const player2 ={
@@ -25,9 +32,9 @@ const player2 ={
     img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon:['axe','knife'],
     attack:attack,
-    changeHP:changeHP,
-    elHP:elHP,
-    renderHP:renderHP
+    changeHP,
+    elHP,
+    renderHP
 };
 player2.attack();
 
@@ -78,24 +85,24 @@ function renderHP(el){
     el.style.width = this.hp+'%';
 }
 
-$randomButton.addEventListener('click',function (){
-    player1.changeHP(getRandom(20));
-    player1.renderHP(player1.elHP());
-    player2.changeHP(getRandom(20));
-    player2.renderHP(player2.elHP());
-
-    if (player1.hp==0||player2.hp==0){
-        $randomButton.disabled=true;
-    }
-
-    if (player1.hp==0&&player1.hp<player2.hp){
-        $arena.appendChild(playerWins(player2.name));
-    }else if (player2.hp==0&&player2.hp<player1.hp){
-        $arena.appendChild(playerWins(player1.name));
-    }else if (player2.hp==0&&player2.hp==0){
-        $arena.appendChild(playerWins());
-    }
-})
+// $randomButton.addEventListener('click',function (){
+//     player1.changeHP(getRandom(20));
+//     player1.renderHP(player1.elHP());
+//     player2.changeHP(getRandom(20));
+//     player2.renderHP(player2.elHP());
+//
+//     if (player1.hp==0||player2.hp==0){
+//         $randomButton.disabled=true;
+//     }
+//
+//     if (player1.hp==0&&player1.hp<player2.hp){
+//         $arena.appendChild(playerWins(player2.name));
+//     }else if (player2.hp==0&&player2.hp<player1.hp){
+//         $arena.appendChild(playerWins(player1.name));
+//     }else if (player2.hp==0&&player2.hp==0){
+//         $arena.appendChild(playerWins());
+//     }
+// })
 
 function playerWins(name){
     const $loseTitle= createElement('div','loseTitle');
@@ -123,3 +130,56 @@ function createReloadButton(){
 
 $arena.appendChild(createPlayer(player1));
 $arena.appendChild(createPlayer(player2));
+
+function  enemyAttack(){
+    const hit=ATTACK[getRandom(3)-1];
+    const defence=ATTACK[getRandom(3)-1];
+    return{
+        value:getRandom(HIT[hit]),
+        hit,
+        defence
+    }
+}
+
+function playerAttack(){
+    const attack = {};
+    for (let item of $formFight){
+        if (item.checked&&item.name=='hit'){
+            attack.value=getRandom(HIT[item.value]);
+            attack.hit= item.value;
+        }
+        if (item.checked&&item.name=='defence'){
+            attack.defence= item.value;
+        }
+        item.checked = false;
+    }
+    return attack;
+}
+$formFight.addEventListener('submit',function (e){
+
+
+    e.preventDefault();
+    console.dir($formFight);
+    const enemy =enemyAttack();
+    const attack = playerAttack();
+
+
+    if (enemy.hit!=attack.defence){
+        player1.changeHP(getRandom(enemy.value));
+        player1.renderHP(player1.elHP());
+    }
+    if (attack.hit!=enemy.defence){
+        player2.changeHP(getRandom(attack.value));
+        player2.renderHP(player2.elHP());
+    }
+
+
+    if (player1.hp==0&&player1.hp<player2.hp){
+        $arena.appendChild(playerWins(player2.name));
+    }else if (player2.hp==0&&player2.hp<player1.hp){
+        $arena.appendChild(playerWins(player1.name));
+    }else if (player2.hp==0&&player2.hp==0){
+        $arena.appendChild(playerWins());
+    }
+
+})
